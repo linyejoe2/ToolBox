d60cd4d64c0a569673ad8b7edcd2e6f41b0f416f
Randy <linyejoe2@gmail.com>
2022-09-30 13:37:57 +0800

    fix cheatSheet.json, update CheatSheet

--- src/views/CheatSheet.vue
+++ src/views/CheatSheet.vue
@@ -1,46 +1,71 @@
 <template>
-  <v-window v-model="onboarding" show-arrows="hover">
-    <v-window-item v-for="(ele,name) in cheatSheetObj">
-      <v-card elevation="2" min-height="400" class="d-flex align-center flex-column
-      justify-center pa-4 ma-2 mb-6  bg-secondary-lighten-2" v-show="!showAns">
-        <div>
-          <h1 class="text-h1 text-center" @click="showAns = !showAns" style="cursor: pointer">
-            {{ name }}
-          </h1>
-        </div>
-        <v-expand-transition>
-          <v-container v-if="showAns" fluid>
-            <v-row dense>
-              <v-col cols="12">
-                <v-card title="Explanation" class="bg-secondary-lighten-1 text-surface-variant">
-                  <v-card-text>
-                    {{ele.explanation}}
-                    <br />
-                    {{ele.explanationTw}}
-                  </v-card-text>
-                </v-card>
-              </v-col>
-              <v-col cols="6">
-                <v-card title="Alias" :text="ele.alias.join(', ')" class="bg-secondary-lighten-1 text-surface-variant">
-                </v-card>
-              </v-col>
-              <v-col cols="6">
-                <v-card title="See Also" class="bg-secondary-lighten-1 text-surface-variant">
-                  <v-card-text>
-                    <v-row dense>
-                      <v-list-item v-for="(url, webName) in ele.seeAlso" :value="webName" :title="webName" :href="url"
-                        target="seeAlso" link>
-                      </v-list-item>
-                    </v-row>
-                  </v-card-text>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-expand-transition>
-      </v-card>
-    </v-window-item>
-  </v-window>
+
+  <v-card flat tile class="bg-secondary-lighten-2 pa-2">
+    <v-window v-model="onboarding">
+      <v-window-item v-for="ele in cheatSheetObj">
+        <v-card elevation="2" min-height="300" class="d-flex align-center flex-column
+      justify-center bg-secondary-lighten-2 pt-3" v-show="!showAns">
+          <div>
+            <h1 class="text-h1 text-center" @click="showAns = !showAns" style="cursor: pointer">
+              {{ ele.name }}
+            </h1>
+          </div>
+          <v-expand-transition>
+            <v-container v-if="showAns" fluid>
+              <v-row dense>
+                <v-col cols="12" md="6">
+                  <v-card title="Explanation" class="bg-secondary-lighten-1 text-surface-variant">
+                    <v-card-text>
+                      {{ele.explanation}}
+                      <!-- <br />
+                    {{ele.explanationTw}} -->
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col cols="12" md="6">
+                  <v-card title="解釋" class="bg-secondary-lighten-1 text-surface-variant">
+                    <v-card-text>
+                      {{ele.explanationTw}}
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col cols="6">
+                  <v-card title="Alias" :text="ele.alias.join(', ')"
+                    class="bg-secondary-lighten-1 text-surface-variant">
+                  </v-card>
+                </v-col>
+                <v-col cols="6">
+                  <v-card title="See Also" class="bg-secondary-lighten-1 text-surface-variant">
+                    <v-card-text>
+                      <v-row dense>
+                        <v-list-item v-for="(url, webName) in ele.seeAlso" :value="webName" :title="webName" :href="url"
+                          target="seeAlso" link>
+                        </v-list-item>
+                      </v-row>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-expand-transition>
+        </v-card>
+      </v-window-item>
+    </v-window>
+
+    <v-card-actions class="justify-space-between">
+      <v-btn variant="plain" icon="mdi-chevron-left" @click="prev"></v-btn>
+      <v-item-group v-model="onboarding" class="text-center" mandatory>
+        <!-- <v-item v-for="ele in cheatSheetObj" v-slot="{ isSelected, toggle }" :value="ele.id">
+          <v-btn :variant="isSelected ? 'outlined' : 'text'" icon="mdi-record" @click="toggle"></v-btn>
+          {{onboarding}}
+        </v-item> -->
+        <v-item>
+          <v-text-field :label="onboarding + '/' +  cheatSheetObj.length.toString()" />
+        </v-item>
+      </v-item-group>
+      <v-btn variant="plain" icon="mdi-chevron-right" @click="next"></v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -48,29 +73,27 @@ import cheatSheetObj from "../public/CheatSheet.json";
 export default ({
   data() {
     return {
-      length: 3,
+      length: cheatSheetObj.data.length - 1,
       onboarding: 0,
-      cheatSheetObj: cheatSheetObj,
-      cheatSheetObj2: {
-        a: {
-          alias: [1, 2, 3]
-        },
-        b: {
-          alias: [1, 2, 3]
-        },
-        er: {
-          alias: [1, 2, 3]
-        },
-        vd: {
-          alias: [1, 2, 3]
-        }
-      },
+      cheatSheetObj: cheatSheetObj.data,
       showAns: false,
-      console2: (text: string) => {
-        console.log(text);
-      }
     }
-  }
+  },
+  computed: {
+    location: () => window.location,
+  },
+  methods: {
+    next() {
+      this.onboarding = this.onboarding + 1 > this.length
+        ? 0
+        : this.onboarding + 1
+    },
+    prev() {
+      this.onboarding = this.onboarding - 1 <= -1
+        ? this.length
+        : this.onboarding - 1
+    },
+  },
 })
 </script>
 
--- src/views/palyGround.vue
+++ src/views/palyGround.vue
@@ -132,6 +132,27 @@
       </v-btn>
     </v-card-actions>
   </v-card>
+  <v-card theme="dark" flat tile>
+    <v-window v-model="onboarding">
+      <v-window-item v-for="n in length" :key="`card-${n}`" :value="n">
+        <v-card height="200" class="d-flex justify-center align-center">
+          <span class="text-h2">
+            Card {{ n }}
+          </span>
+        </v-card>
+      </v-window-item>
+    </v-window>
+
+    <v-card-actions class="justify-space-between">
+      <v-btn variant="plain" icon="mdi-chevron-left" @click="prev"></v-btn>
+      <v-item-group v-model="onboarding" class="text-center" mandatory>
+        <v-item v-for="n in length" :key="`btn-${n}`" v-slot="{ isSelected, toggle }" :value="n">
+          <v-btn :variant="isSelected ? 'outlined' : 'text'" icon="mdi-record" @click="toggle"></v-btn>
+        </v-item>
+      </v-item-group>
+      <v-btn variant="plain" icon="mdi-chevron-right" @click="next"></v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -145,6 +166,8 @@ export default ({
   data() {
     const theme = useTheme();
     return {
+      length: 3,
+      onboarding: 0,
       toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark',
       test: test,
 
@@ -157,6 +180,18 @@ export default ({
         { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' },
       ],
     }
+  },
+  methods: {
+    next() {
+      this.onboarding = this.onboarding + 1 > this.length
+        ? 1
+        : this.onboarding + 1
+    },
+    prev() {
+      this.onboarding = this.onboarding - 1 <= 0
+        ? this.length
+        : this.onboarding - 1
+    },
   },
 })
 </script>
