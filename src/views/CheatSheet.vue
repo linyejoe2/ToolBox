3f391c13b7101f3b0e67f3ccf25e71c6fa696418
RSP_linye <linyejoe2@gmail.com>
2022-10-01 19:04:20 +0800

    update CheatSheet UIUX

--- src/views/CheatSheet.vue
+++ src/views/CheatSheet.vue
@@ -1,13 +1,19 @@
 <template>
-  <v-card flat tile  max-width="1200px" class="bg-secondary-lighten-2 pa-2 mx-auto">
+  <v-card flat tile max-width="1200px" class="bg-secondary-lighten-2 pa-2 mx-auto">
     <v-window v-model="onboarding">
-      <v-window-item v-for="ele in cheatSheetObj">
+      <v-window-item v-for="ele in cheatSheetObj" :key="cheatSheetObj.indexOf(ele)">
         <v-card elevation="2" min-height="300" class="d-flex align-center flex-column
       justify-center bg-secondary-lighten-2 pt-3" v-show="!showAns">
           <div>
-            <h1 class="text-h1 text-center" @click="showAns = !showAns" style="cursor: pointer">
-              {{ ele.name }}
+            <h1 class="text-h1 text-center" style="cursor: pointer">
+              <v-btn variant="plain" disabled/>
+              <span @click="showAns = !showAns" class="ma-4">
+                {{ ele.name }}
+              </span>
+              <v-btn class="bg-primary-lighten-1 " large @click="speak(ele.name)" icon="mdi-volume-high" />
             </h1>
+          </div>
+          <div>
           </div>
           <v-expand-transition>
             <v-container v-if="showAns" fluid>
@@ -29,8 +35,11 @@
                   </v-card>
                 </v-col>
                 <v-col cols="6">
-                  <v-card title="Alias" :text="ele.alias.lingth < 1 ? ele.alias : ele.alias.join(', ')"
+                  <v-card title="Alias" v-if="ele.alias" :text="ele.alias.join(', ')"
                     class="bg-secondary-lighten-1 text-surface-variant">
+                  </v-card>
+                  <v-card title="No Alias" v-if="!ele.alias" class="bg-secondary-lighten-1 text-surface-variant"
+                    text="">
                   </v-card>
                 </v-col>
                 <v-col cols="6">
@@ -52,8 +61,14 @@
     </v-window>
 
     <v-card-actions class="justify-space-between">
-      <v-btn v-if="seens.length" variant="plain" icon="mdi-chevron-left" @click="prev"></v-btn>
-      <v-btn v-if="!seens.length" disable variant="plain" icon="mdi-chevron-left"></v-btn>
+      <v-btn v-if="seens.length" variant="plain" @click="prev">
+        <v-icon left class="mr-2">mdi-chevron-left</v-icon>
+        review privious
+      </v-btn>
+      <v-btn v-if="!seens.length" disabled="true" variant="plain" class="text-secondary-lighten-2">
+        <v-icon left class="mr-2">mdi-chevron-left</v-icon>
+        review privious
+      </v-btn>
       <v-item-group v-model="onboarding" class="text-center" mandatory>
         <v-item>
           <v-text-field :label="onboarding + '/' +  (cheatSheetObj.length - 1).toString()" v-model="changeBoarding"
@@ -62,9 +77,15 @@
             @blur="changeBoarding = undefined" />
         </v-item>
       </v-item-group>
-      <v-btn v-if="seens.length != cheatSheetObj.length - 1" variant="plain" icon="mdi-chevron-right" @click="next">
+      <v-btn v-if="seens.length != cheatSheetObj.length - 1" variant="plain" @click="next">
+        draw a card
+        <v-icon right class="ml-2">mdi-shuffle-variant</v-icon>
       </v-btn>
-      <v-btn v-if="seens.length === cheatSheetObj.length - 1" disable variant="plain" icon="mdi-chevron-right"></v-btn>
+      <v-btn v-if="seens.length === cheatSheetObj.length - 1" disabled variant="plain" class="text-secondary-lighten-2">
+        draw a card
+        <v-icon right class="ml-2">mdi-shuffle-variant</v-icon>
+      </v-btn>
+      <!-- <v-btn v-if="seens.length === cheatSheetObj.length - 1" disable variant="plain" icon="mdi-chevron-right"></v-btn> -->
     </v-card-actions>
   </v-card>
 </template>
@@ -90,6 +111,19 @@ function getPrevSheet() {
     // showAns = false;
     return seens.pop();
   }
+}
+/**
+ * @see (@link https://medium.com/easons-murmuring/%E4%BD%BF%E7%94%A8-javascript-api-%E8%A3%BD%E4%BD%9C%E7%B0%A1%E5%96%AE%E7%9A%84%E8%AA%9E%E9%9F%B3%E6%9C%97%E8%AE%80%E5%99%A8-dabb8230d6da)
+ */
+async function getSpeech(word: string) {
+  const synth = window.speechSynthesis;
+  const utter = new SpeechSynthesisUtterance();
+  // utter.lang = "en-GB";// 語言
+  utter.pitch = 1;
+  utter.voice = (synth.getVoices().filter((ele) => { return ele.name === "Google US English" })[0]);
+  console.log(synth.getVoices());
+  utter.text = word;
+  synth.speak(utter);
 }
 
 export default ({
@@ -126,6 +160,9 @@ export default ({
     },
     log(val) {
       console.log(val);
+    },
+    speak(text: string) {
+      getSpeech(text);
     }
   },
 })
